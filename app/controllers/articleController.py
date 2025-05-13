0b85017070f1ba364e005cdbbf2453d2269aac23
RayaneBICABA <rayanebicaba.dev@gmail.com>
2025-05-13 16:45:34 +0000

    feat(article): add function to get an existing article by id in articleController

--- app/controllers/articleController.py
+++ app/controllers/articleController.py
@@ -4,3 +4,7 @@ from app.models import Article
 #Obtain all articles
 def get_all_articles():
     return Article.query.all()
+
+#Obtain one article by id
+def get_article_by_id(article_id):
+    return Article.query.get(article_id)

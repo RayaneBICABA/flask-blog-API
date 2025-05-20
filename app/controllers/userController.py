ca401e69d4325e2729ae220f997b82ca2d24ddc9
RayaneBICABA <rayanebicaba.dev@gmail.com>
2025-05-20 14:12:12 +0000

    feat(user):add function to create a new user  in userController

--- app/controllers/userController.py
+++ app/controllers/userController.py
@@ -12,3 +12,16 @@ def get_user_by_id(user_id):
 #Obtain one user by email
 def get_user_by_email(email):
     return User.query.filter_by(email=email).first()
+
+#Create a new user
+def create_user(username,email,password_hash,profile_image_url="default.img"):
+    new_user = User(
+        username=username,
+        email=email,
+        password_hash=password_hash,
+        profile_image_url = profile_image_url
+    )
+
+    db.session.add(new_user)
+    db.session.commit()
+    return new_user
